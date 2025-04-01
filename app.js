11661352d9b42ce26f4357c6485cba7215cfa6d2
JITHIN LAKSHMAN <pjithinnavodaya@gmail.com>
2025-04-02 00:04:21 +0530

    edit and route commit

--- app.js
+++ app.js
@@ -3,10 +3,14 @@ const app=express();
 const mongoose=require("mongoose");
 const Listing = require("./models/listing.js");
 const path =require("path");
+const methodOverride=require("method-override");
+
+
 
 app.set("view engine","ejs");
 app.set("Views",path.join(__dirname,"views"));
 app.use(express.urlencoded({extended:true}));
+app.use(methodOverride("_method"));
 
 
 //index Route
@@ -33,6 +37,20 @@ app.get("/listings/:id",async(req,res)=>{
     let {id}=req.params;
     const listing=await Listing.findById(id);
     res.render("listings/show.ejs",{listing});
+});
+
+//Edit Route
+app.get("/listings/:id/edit",async(req,res)=>{
+    let {id}=req.params;
+    const listing=await Listing.findById(id);
+    res.render("listings/edit.ejs",{listing})
+});
+
+//update Route
+app.put("/listings/:id",async(req,res)=>{
+    let {id}=req.params;
+    await Listing.findByIdAndUpdate(id,{...req.body.listing});
+    res.redirect(`/listings/${id}`);
 });
 
 
