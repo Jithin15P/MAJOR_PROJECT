255308aa4279a18683bbf32a48f2cff29cfe9d3f
JITHIN LAKSHMAN <pjithinnavodaya@gmail.com>
2025-04-02 01:11:56 +0530

    ejs-mate

--- app.js
+++ app.js
@@ -53,6 +53,14 @@ app.put("/listings/:id",async(req,res)=>{
     res.redirect(`/listings/${id}`);
 });
 
+//Delete Route
+app.delete("/listings/:id",async(req,res)=>{
+    let {id}=req.params;
+    let deletedListing =await Listing.findByIdAndDelete(id);
+    console.log(deletedListing);
+    res.redirect("/listings")
+})
+
 
 
 //Sample testing route
