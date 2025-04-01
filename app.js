6e4b24bbb3203a17465c835696eb0c6cf2dd58e3
JITHIN LAKSHMAN <pjithinnavodaya@gmail.com>
2025-04-01 20:28:03 +0530

    showroute

--- app.js
+++ app.js
@@ -6,12 +6,20 @@ const path =require("path");
 
 app.set("view engine","ejs");
 app.set("Views",path.join(__dirname,"views"));
+app.use(express.urlencoded({extended:true}));
 
 
 //index Route
 app.get("/listings",async(req,res)=>{
   const allListing= await Listing.find({});
   res.render("listings/index.ejs",{allListing});
+});
+
+//Show route
+app.get("/listings/:id",async(req,res)=>{
+    let {id}=req.params;
+    const listing=await Listing.findById(id);
+    res.render("listings/show.ejs",{listing});
 });
 
 
